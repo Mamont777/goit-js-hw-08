6b05cca3759e18b38b4a150b1edcc95e07150ffc
Sergii Podobrii <podobri@ukr.net>
2023-03-02 18:28:13 +0200

    Task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,8 @@
 import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
 
 const player = new Vimeo(iframe);
@@ -8,11 +10,11 @@ const player = new Vimeo(iframe);
 player.on(
   'timeupdate',
   throttle(function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
+    localStorage.setItem(STORAGE_KEY, data.seconds);
   }, 1000)
 );
 
-const currentTime = localStorage.getItem('videoplayer-current-time');
+const currentTime = localStorage.getItem(STORAGE_KEY);
 
 player
   .setCurrentTime(currentTime)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,87 @@
+// Перший варіант, не розумію чого не працює
 
+// import throttle from 'lodash.throttle';
+
+// const STORAGE_KEY = 'feedback-form-state';
+// const formData = JSON.parse(localStorage.getItem(STORAGE_KEY) || {});
+
+// const refs = {
+//   feedbackForm: document.querySelector('.feedback-form'),
+//   emailInput: document.querySelector('.feedback-form input[name="email"]'),
+//   messageInput: document.querySelector(
+//     '.feedback-form textarea[name="message"]'
+//   ),
+// };
+
+// refs.feedbackForm.addEventListener('submit', onFormSubmit);
+// refs.feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+
+// loadFormData();
+
+// function onFormInput(evt) {
+//   formData[evt.target.name] = evt.target.value;
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// }
+
+// function onFormSubmit(evt) {
+//   evt.preventDefault();
+//   evt.currentTarget.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+//   console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+// }
+
+// function loadFormData() {
+//   const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+//   if (savedMessage) {
+//     const data = JSON.parse(savedMessage);
+//     refs.emailInput.value = data.email;
+//     refs.messageInput.value = data.message;
+//   }
+// }
+
+// Другий варіант
+
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  feedbackForm: document.querySelector('.feedback-form'),
+  emailInput: document.querySelector('.feedback-form input[name="email"]'),
+  messageInput: document.querySelector(
+    '.feedback-form textarea[name="message"]'
+  ),
+};
+
+refs.feedbackForm.addEventListener('submit', onFormSubmit);
+refs.feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+  const formData = {
+    email: refs.emailInput.value,
+    message: refs.messageInput.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  const { email, message } = evt.currentTarget.elements;
+  console.log({ email: email.value, message: message.value });
+}
+
+function loadFormData() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    const data = JSON.parse(savedMessage);
+    refs.emailInput.value = data.email;
+    refs.messageInput.value = data.message;
+  }
+}
+
+loadFormData();
