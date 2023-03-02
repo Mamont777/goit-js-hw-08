e744aed72a8af7f4ab0d41a1782f3cd93e283a1a
Sergii Podobrii <podobri@ukr.net>
2023-03-02 20:25:32 +0200

    Commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,78 +1,35 @@
 // Перший варіант, не розумію чого не працює
 
-// import throttle from 'lodash.throttle';
-
-// const STORAGE_KEY = 'feedback-form-state';
-// const formData = JSON.parse(localStorage.getItem(STORAGE_KEY) || {});
-
-// const refs = {
-//   feedbackForm: document.querySelector('.feedback-form'),
-//   emailInput: document.querySelector('.feedback-form input[name="email"]'),
-//   messageInput: document.querySelector(
-//     '.feedback-form textarea[name="message"]'
-//   ),
-// };
-
-// refs.feedbackForm.addEventListener('submit', onFormSubmit);
-// refs.feedbackForm.addEventListener('input', throttle(onFormInput, 500));
-
-// loadFormData();
-
-// function onFormInput(evt) {
-//   formData[evt.target.name] = evt.target.value;
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// }
-
-// function onFormSubmit(evt) {
-//   evt.preventDefault();
-//   evt.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-//   console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-// }
-
-// function loadFormData() {
-//   const savedMessage = localStorage.getItem(STORAGE_KEY);
-
-//   if (savedMessage) {
-//     const data = JSON.parse(savedMessage);
-//     refs.emailInput.value = data.email;
-//     refs.messageInput.value = data.message;
-//   }
-// }
-
-// Другий варіант Не розумію чому не працює log onFormSubmit
-
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
+let formData;
 
 const refs = {
   feedbackForm: document.querySelector('.feedback-form'),
-  emailInput: document.querySelector('.feedback-form input'),
-  messageInput: document.querySelector('.feedback-form textarea'),
+  emailInput: document.querySelector('.feedback-form input[name="email"]'),
+  messageInput: document.querySelector(
+    '.feedback-form textarea[name="message"]'
+  ),
 };
 
 refs.feedbackForm.addEventListener('submit', onFormSubmit);
 refs.feedbackForm.addEventListener('input', throttle(onFormInput, 500));
 
-function onFormInput() {
-  const formData = {
-    email: refs.emailInput.value,
-    message: refs.messageInput.value,
-  };
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+loadFormData();
+
+function onFormInput(evt) {
+  formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  formData[evt.target.name] = evt.target.value;
+  const message = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, message);
 }
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-
   localStorage.removeItem(STORAGE_KEY);
-  const state = {
-    email: refs.emailInput.value,
-    message: refs.messageInput.value,
-  };
-  console.log(state);
-  evt.target.reset();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  evt.currentTarget.reset();
 }
 
 function loadFormData() {
@@ -84,4 +41,46 @@ function loadFormData() {
   }
 }
 
-loadFormData();
+// import throttle from 'lodash.throttle';
+
+// const STORAGE_KEY = 'feedback-form-state';
+
+// const refs = {
+//   feedbackForm: document.querySelector('.feedback-form'),
+//   emailInput: document.querySelector('.feedback-form input'),
+//   messageInput: document.querySelector('.feedback-form textarea'),
+// };
+
+// refs.feedbackForm.addEventListener('submit', onFormSubmit);
+// refs.feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+
+// function onFormInput() {
+//   const formData = {
+//     email: refs.emailInput.value,
+//     message: refs.messageInput.value,
+//   };
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// }
+
+// function onFormSubmit(evt) {
+//   evt.preventDefault();
+
+//   localStorage.removeItem(STORAGE_KEY);
+//   const state = {
+//     email: refs.emailInput.value,
+//     message: refs.messageInput.value,
+//   };
+//   console.log(state);
+//   evt.target.reset();
+// }
+
+// function loadFormData() {
+//   const savedMessage = localStorage.getItem(STORAGE_KEY);
+//   const data = JSON.parse(savedMessage);
+//   if (savedMessage) {
+//     refs.emailInput.value = data.email;
+//     refs.messageInput.value = data.message;
+//   }
+// }
+
+// loadFormData();
